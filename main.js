db962bff156e4e28d076f233e0992c711c780e1a
Jean Lima <jcoliveiralima19@gmail.com>
2026-02-03 13:50:02 -0300

    Criando formulário de card e adicionando input de status

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
+const itensStatus = document.querySelectorAll(".status-item");
 const btnExcluir = document.querySelectorAll(".btn-excluir");
+
+itensStatus.forEach((elemento) => {
+    elemento.addEventListener('click', () => {
+        if (!elemento.classList[2]) {
+            itensStatus.forEach((item) => {
+                item.classList.remove('status-selecionado');
+            })
+
+            elemento.classList.add('status-selecionado');
+        };
+    });
+});
 
 btnExcluir.forEach((elemento) => {
     elemento.addEventListener('click', () => {
         elemento.parentElement.parentElement.remove();
-    })
-})
+    });
+});
